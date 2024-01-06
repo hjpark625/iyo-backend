37fa04efbc612569831e3883a59714c3f1747924
hjpark625 <zero950@naver.com>
2024-01-06 19:38:14 +0900

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { LoggingMiddleware } from '@/middleware/logging.middleware';
+import type { MiddlewareConsumer } from '@nestjs/common';
+import type { MongooseModuleFactoryOptions } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): MongooseModuleFactoryOptions => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@/app.module';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(PORT);
+}
+bootstrap();
