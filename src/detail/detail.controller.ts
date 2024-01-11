23db29bfc942d9a22d6c58b13c4d941af6db358b
hjpark625 <zero950@naver.com>
2024-01-11 21:58:05 +0900

    feat: 상세(개별)데이터 조회 완성

--- src/detail/detail.controller.ts
+++ src/detail/detail.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpException, HttpStatus, Param } from '@nestjs/common';
 import { DetailService } from '@/detail/detail.service';
 
 @Controller('detail')
 export class DetailController {
   constructor(private readonly detailService: DetailService) {}
 
-  @Get()
+  @Get(':storename')
   @HttpCode(HttpStatus.OK)
-  getDetail(): string {
-    return this.detailService.getHello();
+  async getDetailData(@Param('storename') storeName: string) {
+    try {
+      const result = this.detailService.getDetailData(storeName);
+      return result;
+    } catch (err: unknown) {
+      if (err instanceof HttpException) {
+        throw new HttpException({ message: err.getResponse() }, err.getStatus());
+      }
+    }
   }
 }
--- src/detail/detail.service.ts
+++ src/detail/detail.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Detail } from '@/detail/detail.schema';
+import { DetailModel } from '@/dto/detail.dto';
 
 @Injectable()
 export class DetailService {
-  getHello(): string {
-    return 'This is Detail';
+  constructor(@InjectModel(Detail.name) private detailModel: DetailModel) {}
+
+  async getDetailData(storeName: string) {
+    if (!storeName.trim()) throw new HttpException('정확한 상점 이름을 입력해주세요', HttpStatus.BAD_REQUEST);
+
+    try {
+      const detailData = await this.detailModel.findOne({ engName: storeName }).exec();
+
+      if (detailData == null) throw new HttpException('상점 정보를 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
+
+      return {
+        data: detailData,
+      };
+    } catch (err: unknown) {
+      if (err instanceof HttpException) {
+        throw new HttpException(err.getResponse(), err.getStatus());
+      } else {
+        throw new HttpException(`${err}`, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
   }
 }
