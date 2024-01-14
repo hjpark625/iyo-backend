abd8892a66c81424c64058dbe8ab44ca15d761bc
hjpark625 <zero950@naver.com>
2024-01-14 17:44:10 +0900

    refactor: detail 데이터 스키마 변경

--- src/detail/detail.schema.ts
+++ src/detail/detail.schema.ts
@@ -21,6 +21,9 @@ export class Detail extends Document {
   @Prop({ type: String, required: false, default: null })
   detailAddress: string | null;
 
+  @Prop({ type: String, required: false, default: null })
+  socialLinks!: string | null;
+
   @Prop({ type: Object, required: false, default: null })
   nearestRoute: {
     subwayLine: string[] | null;
@@ -37,14 +40,14 @@ export class Detail extends Document {
   @Prop({ type: String, required: false, default: null })
   phoneNumber: string | null;
 
-  @Prop({ type: Number, required: true })
-  lat: number;
-
-  @Prop({ type: Number, required: true })
-  lng: number;
+  @Prop({ type: Object, required: true })
+  coord: { lat: number; lng: number };
 
   @Prop({ type: String, required: false, default: null })
   description: string | null;
+
+  @Prop({ type: String, required: false, default: null })
+  introduce: string | null;
 
   @Prop({ type: Date, required: true })
   updatedAt: Date;
