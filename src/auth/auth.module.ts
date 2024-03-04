43804f1da471c6caa306316cd49c09b164833eb9
hjpark625 <zero950@gmail.com>
2024-03-04 11:18:02 +0900

    feat: JWT모듈, DB모듈 활성화

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from '@/auth/auth.controller';
+import { User, UserSchema } from '@/auth/auth.schema';
+import type { JwtModuleOptions } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): JwtModuleOptions => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { algorithm: 'HS256' },
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
