7f1b6a72b685eecf7e029c1525d94fa950c01569
hjpark625 <zero950@gmail.com>
2024-03-05 10:14:06 +0900

    chore: import 순서 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
+import mongoose from 'mongoose';
 import { User } from '@/auth/auth.schema';
 import { IUserModel } from '@/dto/auth.dto';
 import type { IDecodedTokenInfo } from '@/dto/auth.dto';
